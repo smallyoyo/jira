67082edd72899bf409790d4ec5a0814076f457c1
David <1421296065@qq.com>
2022-12-26 21:08:35 +0800

    fix

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+import qs from "qs";
+import {cleanObject, useDebounce, useMount} from "../utils";
+
+
+const apiUrl = process.env.REACT_APP_API_URL;
+export const ProjectListScreen = () => {
+
+    const [param, setParam] = useState({
+        name : '',
+        personId: ''
+    })
+    const [list, setList] = useState([])
+
+    const [users, setUsers] = useState([])
+
+    const debouncedParam = useDebounce(param, 500)
+
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [debouncedParam])
+
+    useMount(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json())
+            }
+        })
+    })
+
+    return <div>
+        <SearchPanel param={param} setParam={setParam} users={users}/>
+        <List list={list} users={users}/>
+    </div>
+
+
+}
